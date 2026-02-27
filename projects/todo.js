f60090e5553adf70a6256a27fea506e227bfaf2c
A Damian <duckjopa200@gmail.com>
2026-02-27 03:37:50 +0200

    TodoList added

---
+++ projects/todo.js
@@ -0,0 +1,89 @@
+const todoInput = document.querySelector('#todoInput');
+const saveBtn = document.querySelector('#saveBtn');
+const todoList = document.querySelector('#todoList');
+const username = prompt("Enter username..")
+const currentUser = document.querySelector('#currentUser').textContent = username
+
+const DB_URL = "https://tinkr.tech/sdb/todo_damian"; 
+
+//Getting todo
+
+async function getTodo() {
+  const response = await fetch(DB_URL);
+  const elements = await response.json();
+
+  todoList.innerHTML = '';
+
+  elements
+  .filter(todo => todo.user === username)
+  .forEach(todo => {
+    const todoDiv = document.createElement('div');
+    todoDiv.className = 'todoDiv';
+
+    const textSpan = document.createElement('span'); 
+    textSpan.textContent = todo.text;
+
+    const actionsDiv = document.createElement('div');
+    actionsDiv.className = 'actionsDiv';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.className = 'deleteBtn'
+
+    deleteBtn.addEventListener('click', async () => {
+      await fetch(`${DB_URL}/${todo.id}`, { method: 'DELETE' });
+      getTodo();
+    });
+
+    const doneDiv = document.createElement('div');
+    doneDiv.className = 'doneDiv';
+
+    const doneChk = document.createElement('p');
+    doneChk.className = 'p';
+    doneChk.textContent = 'Done:';
+
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.className = 'checkBox'
+    checkBox.checked = todo.done;
+
+    checkBox.addEventListener('change', async () => {
+      await fetch(`${DB_URL}/${todo.id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ user: username, text: todo.text, done: checkBox.checked })
+      });
+    });
+
+    doneDiv.appendChild(doneChk);
+    doneDiv.appendChild(checkBox);
+
+    actionsDiv.appendChild(doneDiv)
+    actionsDiv.appendChild(deleteBtn);
+
+    todoDiv.appendChild(textSpan)
+    todoDiv.appendChild(actionsDiv)
+
+    todoList.appendChild(todoDiv);
+  });
+}
+
+//Saving todo
+
+async function saveTodo() {
+  const text = todoInput.value;
+  if (!text) return;
+
+  await fetch(DB_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ user:username, text, done: false})
+  });
+
+  todoInput.value = '';
+  getTodo();
+}
+
+saveBtn.addEventListener('click', saveTodo);
+
+getTodo();
